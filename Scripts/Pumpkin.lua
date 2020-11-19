06e786c316acdc9dbf4f324f535488e86b75bd19
POTATO9023 <74697725+POTATO9023@users.noreply.github.com>
2020-11-18 22:58:25 -0600

    Update Pumpkin.lua

--- Scripts/Pumpkin.lua
+++ Scripts/Pumpkin.lua
@@ -1,9 +1,9 @@
 -- User-changeable variables
-local pumpkin_colour = {["r"] = 0x0, ["g"] = 0xf, ["b"] = 0xf}
+local pumpkin_colour = {["r"] = 0xf, ["g"] = 0x2, ["b"] = 0x0}
 
 --
 Lighting.SetFlashingSpeed(0)
-Lighting.SetBreathingModeEnabled(false)
+Lighting.SetBreathingModeEnabled(true)
 
 for i = 1, 8 do
     Lighting.SetColour(i, pumpkin_colour.r, pumpkin_colour.g, pumpkin_colour.b)
